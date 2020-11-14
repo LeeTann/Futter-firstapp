1a388be23c7102ffc9698a4cedd81781c52f01bc
ltann <leeroyleeroy18@gmail.com>
2020-11-14 15:31:16 -0800

    Added TextField

--- lib/main.dart
+++ lib/main.dart
@@ -23,16 +23,45 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Hello Leeroy')),
-        body: Column(
-          children: <Widget>[TestWidget(), TestWidget(), TestWidget()],
-        ));
+        appBar: AppBar(title: Text('Hello Leeroy')), body: TextInputWidget());
   }
 }
 
-class TestWidget extends StatelessWidget {
+class TextInputWidget extends StatefulWidget {
+  @override
+  __TextInputWidgetState createState() => __TextInputWidgetState();
+}
+
+class __TextInputWidgetState extends State<TextInputWidget> {
+  final controller = TextEditingController();
+  String text = "";
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  void changeText(text) {
+    if (text == "Hello World!") {
+      controller.clear();
+      text = "";
+    }
+    setState(() {
+      this.text = text;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("hello again!");
+    return Column(children: <Widget>[
+      TextField(
+        controller: this.controller,
+        decoration: InputDecoration(
+            prefixIcon: Icon(Icons.message), labelText: "Type a message:"),
+        onChanged: (text) => this.changeText(text),
+      ),
+      Text(this.text)
+    ]);
   }
 }
